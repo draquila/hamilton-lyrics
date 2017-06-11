1cb66019a9cb90baf8a20c0aedcc2517363bf43a
Ian Su <iansu@optusnet.com.au>
2017-06-12 03:08:20 +1000

    removed offset and made 1-01 more consistent

---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+find . -not \( -name .svn -prune -o -name .git -prune \) -name *.lrc -type f -print0 | xargs -0 dos2unix
+find . -not \( -name .svn -prune -o -name .git -prune \) -name *.lrc -type f -print0 | xargs -0 sed -i '' -E "s/[[:space:]]*$//"
