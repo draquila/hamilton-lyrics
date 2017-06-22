377e527d8408961b69c0e82e53bbcb44f7bbe66e
Ian Su <ian.su@sirca.org.au>
2017-06-22 22:32:06 +1000

    Minor fixes (#7)
    * More minor fixes
    
    * minor adjustments
    
    * fix clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 find . -not \( -name .svn -prune -o -name .git -prune \) -name *.lrc -type f -print0 | xargs -0 dos2unix
-find . -not \( -name .svn -prune -o -name .git -prune \) -name *.lrc -type f -print0 | xargs -0 sed -i '' -E "s/[[:space:]]*$//"
+find . -not \( -name .svn -prune -o -name .git -prune \) -name *.lrc -type f -print0 | xargs -0 /usr/bin/sed -i '' -E "s/[[:space:]]*$//"
